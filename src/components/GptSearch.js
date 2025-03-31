97dffa8870ae45e7c2a1c8422971245e4e373165
Raghib <rahgib@gmail.com>
2025-04-01 02:00:04 +0530

    fixed

--- src/components/GptSearch.js
+++ src/components/GptSearch.js
@@ -7,7 +7,7 @@ const GptSearch = () => {
     <>
         <div className='fixed -z-10 '>
 
-<img className='h-screen object-cover w-screen' src="https://assets.nflxext.com/ffe/siteui/vlv3/04bef84d-51f6-401e-9b8e-4a521cbce3c5/null/IN-en-20240903-TRIFECTA-perspective_0d3aac9c-578f-4e3c-8aa8-bbf4a392269b_large.jpg"/>
+<img className='h-screen object-cover w-screen' src="https://analyticsindiamag.com/wp-content/uploads/2019/05/apps.55787.9007199266246365.687a10a8-4c4a-4a47-8ec5-a95f70d8852d-1540x866.jpg"/>
 </div>
     <div className=''>
         <GptSearchBar/>
